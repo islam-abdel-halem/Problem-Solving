0877b917610a0b93ccad8b1a86fff4116847494a
ESLAM <102685868+ESLAM80@users.noreply.github.com>
2023-03-31 15:43:55 +0200

    add

--- C++/Sheet Assiut/Sheet#3-Array/W. Mirror Array.cpp
+++ C++/Sheet Assiut/Sheet#3-Array/W. Mirror Array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int n, m, i, j;
-    cin >> n, m;
+    cin >> n >> m;
     int arr[n][m];
     for (i = 0; i < n; i++)
     {
@@ -23,7 +23,7 @@ int main()
     {
         for (j = 0; j < m; j++)
         {
-            cout << arr[i][j]<<' ';
+            cout << arr[i][j] << ' ';
         }
         cout << endl;
     }
